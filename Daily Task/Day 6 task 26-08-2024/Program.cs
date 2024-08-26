0760242203b661da6d870b9ca7a7b2f95fc25dd7
siddarthhh <114459478+siddarthhh@users.noreply.github.com>
2024-08-26 16:43:14 +0530

    Add files via upload

---
+++ Daily Task/Day 6 task 26-08-2024/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Data.SqlClient;
+using System.Runtime.InteropServices;
+
+internal class Program
+{
+    static int inee;
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        SqlConnection conn = new SqlConnection("data source =PTSQLTESTDB01;database=Sports_Sidd;integrated security=true;");
+
+
+        conn.Open();
+
+
+        SqlCommand cmd = new SqlCommand("select * from Product", conn);
+
+
+
+        SqlDataReader sdr = cmd.ExecuteReader();
+
+        while (sdr.Read())
+        {
+            Console.WriteLine(sdr[0].ToString() + " " + sdr[1].ToString());
+        }
+        static void display()
+        {
+
+        }
+        conn.Close();
+    }
+}
