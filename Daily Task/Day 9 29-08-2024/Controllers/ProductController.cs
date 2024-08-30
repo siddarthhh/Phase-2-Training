ee5c6a469237878441d79824c68a2135ffeac7cc
siddarthhh <114459478+siddarthhh@users.noreply.github.com>
2024-08-30 16:45:24 +0530

    Add files via upload

---
+++ Daily Task/Day 9 29-08-2024/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_ADO.Data_Access;
+using MVC_ADO.Models;
+using System.Collections;
+
+namespace MVC_ADO.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductDataAccess pda = new ProductDataAccess();
+        public IActionResult Index()
+        {
+            List<ProductModel> data = pda.fetch();
+            return View(data);
+        }
+    }
+}
---
+++ Daily Task/Day 9 29-08-2024/Controllers/ProductsController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MVC_ADO.Data_Access;
+using MVC_ADO.Models;
+
+namespace MVC_ADO.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: ProductsController
+        ProductDataAccess pda = new ProductDataAccess();
+
+        public IActionResult Index()
+        {
+            List<ProductModel> data = pda.fetch();
+
+            return View(data);
+        }
+
+        // GET: ProductsController/Details/5
+        public ActionResult Details(int id)
+        {
+            ProductModel pm = pda.search(id);
+            return View(pm);
+        }
+
+        // GET: ProductsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ProductModel product)
+        {
+            try
+            {
+                pda.insert(product);  
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            ProductModel model = pda.search(id);
+            return View(model);
+        }
+
+        // POST: ProductsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, ProductModel product)
+        {
+            try
+            {
+                pda.update(product);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            ProductModel model = pda.search(id);
+            return View(model);
+        }
+
+        // POST: ProductsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id,ProductModel product)
+        {
+            try
+            {
+                pda.delete(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
