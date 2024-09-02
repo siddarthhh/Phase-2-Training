712d36f5c9e41b9f5b2293c289bba73fea6d5d4a
siddarthhh <114459478+siddarthhh@users.noreply.github.com>
2024-09-03 00:47:23 +0530

    Add files via upload

---
+++ Daily Task/day 11 Task 3-9-2024/RepPattern/Repository/IHotel.cs
@@ -0,0 +1,11 @@
+﻿using RepoPatternAssignment.Models;
+
+namespace RepoPatternAssignment.Repository
+{
+    public interface IHotel
+    {
+        IEnumerable<HotelModel> GetAll();
+
+        void AddHotel(HotelModel hotel);
+    }
+}
