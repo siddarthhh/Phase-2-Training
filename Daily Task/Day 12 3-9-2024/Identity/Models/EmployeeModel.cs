ddf529d82bb6f4aa07088d174fe42d4469e895b5
siddarthhh <114459478+siddarthhh@users.noreply.github.com>
2024-09-04 11:58:23 +0530

    Add files via upload

---
+++ Daily Task/Day 12 3-9-2024/Identity/Models/EmployeeModel.cs
@@ -0,0 +1,13 @@
+﻿namespace IdentityEX2.Models
+{
+   
+        public class Employee
+        {
+            public int EmpId { get; set; }
+            public string EmployeeName { get; set; }
+            public int EmployeeAge { get; set; }
+            public int AddressId { get; set; }
+        }
+   
+
+}
